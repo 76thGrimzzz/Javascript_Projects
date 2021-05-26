366d96c5723dcfa9709e087a9bbd5b4598cd87e5
76thGrimzzz <pstreet.banerjee@gmail.com>
2021-05-26 08:03:20 +0545

    initial commit

---
+++ Custom Music Player(incomplete)/script.js
@@ -0,0 +1,6 @@
+let previous = document.getElementsByClassName('prev');
+let play = document.getElementsByClassName('play');
+let next = document.getElementsByClassName('next');
+let duration = document.getElementsByClassName('duration');
+let track_image = document.getElementsByClassName('track_image');
+let autoplay = document.getElementsByClassName('auto');
