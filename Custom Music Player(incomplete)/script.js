3a22676deb2fddf8c919d424480c5b2e6fad2dcb
76thGrimzzz <77434158+76thGrimzzz@users.noreply.github.com>
2021-05-27 21:31:15 +0545

    Delete Custom Music Player(incomplete) directory

--- Custom Music Player(incomplete)/script.js
+++
@@ -1,6 +0,0 @@
-let previous = document.getElementsByClassName('prev');
-let play = document.getElementsByClassName('play');
-let next = document.getElementsByClassName('next');
-let duration = document.getElementsByClassName('duration');
-let track_image = document.getElementsByClassName('track_image');
-let autoplay = document.getElementsByClassName('auto');
